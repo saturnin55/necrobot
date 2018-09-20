389e4cede5dc643dffbc89fe86e0391c77d91aba
Simon Chen <khold6458@gmail.com>
2018-09-20 08:16:56 -0400

    warn on channel not found

--- run_lunkybot.py
+++ run_lunkybot.py
@@ -1,9 +1,15 @@
 from necrobot.botbase import server
 from necrobot.spelunky.lunkymainchannel import MainBotChannel
+from necrobot.util import console
 from necrobot import logon
 
+
 async def load_lunkybot_config(necrobot):
-    server.main_channel = server.find_channel(channel_name='lunkybot')
+    MAIN_CHANNEL_NAME = 'lunkybot'
+
+    server.main_channel = server.find_channel(channel_name=MAIN_CHANNEL_NAME)
+    if server.main_channel is None:
+        console.warning(f'Could not find the "{MAIN_CHANNEL_NAME}" channel.')
     necrobot.register_bot_channel(server.main_channel, MainBotChannel())
 
 
